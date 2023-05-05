dab9d39499a37219e24af5fc8109df58cdcc1e2a
Felipe Ferreira <felipeferreira2412@gmail.com>
2023-05-05 01:02:05 -0300

    DTO

--- backend/src/main/java/com/ferreira/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/ferreira/dscatalog/services/CategoryService.java
@@ -1,22 +1,26 @@
 package com.ferreira.dscatalog.services;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.ferreira.dscatalog.dto.CategoryDTO;
 import com.ferreira.dscatalog.entities.Category;
 import com.ferreira.dscatalog.repositories.CategoryRepository;
 
 @Service
 public class CategoryService {
-    
+
     @Autowired
     private CategoryRepository repository;
 
-    @Transactional(readOnly = true) 
-    public List<Category> findAll(){
-        return repository.findAll();
+    @Transactional(readOnly = true)
+    public List<CategoryDTO> findAll() {
+        List<Category> list = repository.findAll();
+        return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
     }
 }

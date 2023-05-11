049f60e942fdb6584ee4ecd67fcc8874612435c0
Felipe Ferreira <felipeferreira2412@gmail.com>
2023-05-11 00:19:44 -0300

    Update category

--- backend/src/main/java/com/ferreira/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/ferreira/dscatalog/services/CategoryService.java
@@ -13,6 +13,8 @@ import com.ferreira.dscatalog.entities.Category;
 import com.ferreira.dscatalog.repositories.CategoryRepository;
 import com.ferreira.dscatalog.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class CategoryService {
 
@@ -38,5 +40,17 @@ public class CategoryService {
         entity.setName(dto.getName());
         entity = repository.save(entity);
         return new CategoryDTO(entity);
+    }
+
+    @Transactional
+    public CategoryDTO update(Long id, CategoryDTO dto) {
+        try {
+            Category entity = repository.getReferenceById(id);
+            entity.setName(dto.getName());
+            entity = repository.save(entity);
+            return new CategoryDTO(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Id not found");
+        }
     }
 }

d5976351a98c808c6603f800eafc94998ae3f35a
Felipe Ferreira <felipeferreira2412@gmail.com>
2023-08-18 20:40:18 -0300

    User CRUD

---
+++ backend/src/main/java/com/ferreira/dscatalog/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.ferreira.dscatalog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable());
+        http.sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.authorizeHttpRequests(req -> req.requestMatchers("/h2-console/").permitAll());
+        http.headers(headers -> headers.frameOptions(frame -> frame.sameOrigin()));
+        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        return http.build();
+    }
+}
